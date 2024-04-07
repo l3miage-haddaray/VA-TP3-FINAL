558106a6a1ea0d56cb6cfe85eeeff340f665b31a
L3miage-haddaray <rayane.haddad@etu.univ-grenoble-alpes.fr>
2024-04-07 23:32:14 +0200

    ajout endpoint dans rest api

--- server/src/main/java/fr/uga/l3miage/spring/tp3/controller/CandidateController.java
+++ server/src/main/java/fr/uga/l3miage/spring/tp3/controller/CandidateController.java
@@ -6,6 +6,8 @@ import fr.uga.l3miage.spring.tp3.services.CandidateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
+import java.util.Set;
+
 @Controller
 @RequiredArgsConstructor
 public class CandidateController implements CandidateEndpoints {
@@ -14,5 +16,10 @@ public class CandidateController implements CandidateEndpoints {
     @Override
     public Double getCandidateAverage(Long candidateId) {
         return candidateService.getCandidateAverage(candidateId);
+    }
+
+    @Override
+    public Boolean addStudentsToTestCenter(Long centerId, Set<Long> studentIds) {
+        return null;
     }
 }
