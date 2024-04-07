dde37abfe2efe14aacf47569872de66db71198b9
L3miage-haddaray <rayane.haddad@etu.univ-grenoble-alpes.fr>
2024-04-07 23:26:57 +0200

    ajout endpoint dans rest api

--- rest-api/src/main/java/fr/uga/l3miage/spring/tp3/endpoints/CandidateEndpoints.java
+++ rest-api/src/main/java/fr/uga/l3miage/spring/tp3/endpoints/CandidateEndpoints.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
 
 @Tag(name = "Gestion des candidat")
@@ -23,6 +24,14 @@ public interface CandidateEndpoints {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{candidateId}/average")
     Double getCandidateAverage(@PathVariable Long candidateId);
+
+    @Operation(description = "Ajouter une collection d'étudiants à un centre de test")
+    @ApiResponse(responseCode = "202", description = "Les étudiants ont été ajoutés avec succès")
+    @ApiResponse(responseCode = "404", description = "Le centre de test ou l'étudiant n'a pas été trouvé", content = @Content(schema = @Schema(implementation =  CandidatNotFoundResponse.class), mediaType = MediaType.APPLICATION_JSON_VALUE))
+    @ApiResponse(responseCode = "400", description = "Requête incorrecte, vérification métier non passée", content = @Content(schema = @Schema(implementation = String.class), mediaType = MediaType.APPLICATION_JSON_VALUE))
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping("/{centerId}/add-students")
+    Boolean addStudentsToTestCenter(@PathVariable(name="centerId") Long centerId, @RequestBody Set<Long> studentIds);
 
 
    }
